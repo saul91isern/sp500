5da0f5fe2a0ab40bb63c9e223139506ca8f3f29f
saul.isern <saul.isern@bluetab.net>
2019-12-30 13:33:35 +0100

    feat: first rl algorithm train

--- rl/agent.py
+++ rl/agent.py
@@ -4,6 +4,7 @@ from collections import deque
 
 import numpy as np
 import random
+import math
 
 class Agent:
     def __init__(self, state_size, amount_features, learning=True, epsilon=1.0, epsilon_min=0.01, epsilon_decay=-0.015, gamma=0.95):
@@ -18,13 +19,13 @@ class Agent:
         self.__t = 0
         self.__learning = learning
         self.inventory = []
-        self.__model = self.__model()
+        self.model = self.__model()
     
     def __model(self):
         model = Sequential()
 
         model.add(GRU(
-            units=64,
+            units=32,
             activation="tanh",
             return_sequences=True,
             dropout=0.2,
@@ -33,14 +34,13 @@ class Agent:
         )
 
         model.add(GRU(
-            units=64,
+            units=32,
             activation="tanh",
             return_sequences=False,
             dropout=0.2
             )
         )
 
-        model.add(Dense(32, activation="relu"))
         model.add(Dense(16, activation="relu"))
         model.add(Dense(8, activation="relu"))
         model.add(Dense(self.__action_size, activation="linear"))
@@ -56,8 +56,8 @@ class Agent:
         if self.__learning and random.random() <= self.__epsilon:
             return random.randrange(self.__action_size)
 
-        options = self.__model.predict(state)
-        print("Options: ", options)
+        options = self.model.predict(self.__with_valid_shape(state))
+
         # Return the index of the max value
         return np.argmax(options[0])
 
@@ -70,12 +70,17 @@ class Agent:
         for state, action, reward, next_state, done in mini_batch:
             target = reward
             if not done:
-                target = reward + self.__gamma * np.amax(self.__model.predict(next_state)[0])
+                target = reward + self.__gamma * np.amax(self.model.predict(self.__with_valid_shape(next_state))[0])
             
-            target_f = self.__model.predict(state)
+            target_f = self.model.predict(self.__with_valid_shape(state))
             target_f[0][action] = target
-            self.__model.fit(state, target_f, epochs=1, verbose=0)
+            self.model.fit(self.__with_valid_shape(state), target_f, epochs=1, verbose=0)
         
         if self.__epsilon > self.__epsilon_min:
             self.__epsilon = math.exp(self.__epsilon_decay * self.__t)
             self.__t =  self.__t + 1
+
+    def __with_valid_shape(self, state):
+        if len(state.shape) == 2:
+            return np.reshape(state, (1, state.shape[0], state.shape[1]))
+        return state
